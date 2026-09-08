af2f09012440abd59b32cc95222f53663500218b
andromeda951 <andromeda951@gmail.com>
2026-09-08 18:34:47 +0700

    update

---
+++ 6_casting.py
@@ -0,0 +1,10 @@
+x = 1             
+y = 2.8            
+z = int("312")
+
+z = z + 2
+print(z)
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
---
+++ input.py
@@ -0,0 +1,18 @@
+print('inputkan alas')
+alas = int(input())
+
+print('inputkan tinggi')
+tinggi = int(input())
+
+print(f'luas = {0.5 * alas * tinggi}')
+
+# name = input()
+# print(f"nama: {name}")
+
+
+# p = int(input())
+# l = int(input())
+
+# luas = p * l
+
+# print(f"luas = {luas}")
